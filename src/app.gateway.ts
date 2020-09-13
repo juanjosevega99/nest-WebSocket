93aff3dc71b8d0630c30d2aa811a05b6885a99b2
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-13 16:10:39 -0500

    [FEATURE] first improve

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,7 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 
 @WebSocketGateway()
 export class AppGateway {
+  
+  private logger: Logger = new Logger('AppGateway')
+
+  afterInit(server : any) {
+    this.logger.log('Initialized')
+  }
+  
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
