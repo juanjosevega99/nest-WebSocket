d3f5ff68ca1c1d3cf542712d9924423bd32c4de7
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-14 09:23:18 -0500

    [ADD] chat gateway

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,23 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayInit {
+  @WebSocketServer() wss: Server;
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(client: Socket, message: { sender: string; message: string }) {
+    this.wss.emit('chatToClient', message);
+  }
+}
